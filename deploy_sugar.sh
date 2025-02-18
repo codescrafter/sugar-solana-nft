fe21b7750c4afd9700e0ca9144050e13cbc80116
codescrafter <abubakarafzal444@gmail.com>
2025-02-18 13:21:58 +0500

    mint now added

--- deploy_sugar.sh
+++ deploy_sugar.sh
@@ -35,13 +35,23 @@ sugar deploy
 echo "Verifying..."
 sugar verify
 
-# echo "Minting..."
-# sugar mint
+# Mint the entire collection
+# echo "Minting the entire collection..."
+# TOTAL_NFTS=10  # Change this to match the number in your JSON configuration
+# for ((i=1; i<=TOTAL_NFTS; i++))
+# do
+#   echo "Minting NFT $i of $TOTAL_NFTS..."
+#   sugar mint
+#   if [ $? -ne 0 ]; then
+#     echo "Minting failed for NFT $i. Exiting."
+#     exit 1
+#   fi
+# done
 
 echo "Adding guard..."
 sugar guard add
 
-echo "Deployment complete."
+echo "Deployment and minting complete."
 
 # Delete files after script finishes (success or failure)
 echo "Cleaning up..."
